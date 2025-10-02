c1de18bd7800472b5938fd01892debd34e5079bb
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 01:25:56 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -1,37 +1,22 @@
 ymaps.ready(init);
 
 function init() {
-    // Создаем карту с тёмной темой
+    // Создаем пустую карту без центра и зума, чтобы избежать "прыжков"
     var myMap = new ymaps.Map("map", {
         controls: [],
-        type: "yandex#darkMap" // тёмная тема карты
     });
 
-    // Геокодируем Воронеж, чтобы получить его границы
+    // Получаем границы города Воронеж через геокодер
     ymaps.geocode("Воронеж", { results: 1 }).then(function (res) {
         var city = res.geoObjects.get(0);
-        var bounds = city.properties.get('boundedBy');
+        var bounds = city.properties.get('boundedBy'); // границы города
+        myMap.setBounds(bounds, { checkZoomRange: true, zoomMargin: 20 });
 
-        // Расширяем границы на 30% для показа окрестностей и трасс
-        var latDiff = (bounds[1][0] - bounds[0][0]) * 0.3;
-        var lonDiff = (bounds[1][1] - bounds[0][1]) * 0.3;
-        var extendedBounds = [
-            [bounds[0][0] - latDiff, bounds[0][1] - lonDiff],
-            [bounds[1][0] + latDiff, bounds[1][1] + lonDiff]
-        ];
-
-        // Устанавливаем расширенные границы на карту
-        myMap.setBounds(extendedBounds, { checkZoomRange: true, zoomMargin: 20 });
-
-        // Добавляем слой пробок с дорожными событиями
-        var trafficLayer = new ymaps.traffic.layer.Actual({
-            infoLayerShown: true
-        });
-        myMap.layers.add(trafficLayer);
-
-        // Автообновление пробок каждые 2 минуты
-        setInterval(function () {
-            trafficLayer.reload();
-        }, 120000); // 120000 мс = 2 минуты
+        // После установки границ подключаем провайдер пробок
+        var actualProvider = new ymaps.traffic.provider.Actual(
+            {},
+            { infoLayerShown: true } // включаем дорожные события
+        );
+        actualProvider.setMap(myMap);
     });
 }
