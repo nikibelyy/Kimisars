c5b64f752686beb03ccd3537a8a8a9ecb822f24e
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 01:20:48 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -1,44 +1,37 @@
 ymaps.ready(init);
 
 function init() {
-    // Создаем карту со светлой темой
+    // Создаем карту с тёмной темой
     var myMap = new ymaps.Map("map", {
-        center: [51.6608, 39.2003], // Воронеж
-        zoom: 11,
         controls: [],
-        type: "yandex#map" // светлая карта
+        type: "yandex#darkMap" // тёмная тема карты
     });
 
-    // Добавляем слой пробок с дорожными событиями (ДТП, ремонт и др.)
-    var trafficLayer = new ymaps.traffic.layer.Actual({
-        infoLayerShown: true
-    });
-    myMap.layers.add(trafficLayer);
-
-    // Автообновление пробок каждые 1 минуту
-    setInterval(function() {
-        trafficLayer.reload();
-    }, 60000);
-
-    // Геокодируем Воронеж для расширенного охвата
-    ymaps.geocode("Воронеж", { results: 1 }).then(function(res) {
+    // Геокодируем Воронеж, чтобы получить его границы
+    ymaps.geocode("Воронеж", { results: 1 }).then(function (res) {
         var city = res.geoObjects.get(0);
-        if (city) {
-            var bounds = city.properties.get('boundedBy');
+        var bounds = city.properties.get('boundedBy');
 
-            // Расширяем границы на 40% для окрестностей и трасс
-            var latDiff = (bounds[1][0] - bounds[0][0]) * 0.4;
-            var lonDiff = (bounds[1][1] - bounds[0][1]) * 0.4;
-            var extendedBounds = [
-                [bounds[0][0] - latDiff, bounds[0][1] - lonDiff],
-                [bounds[1][0] + latDiff, bounds[1][1] + lonDiff]
-            ];
+        // Расширяем границы на 30% для показа окрестностей и трасс
+        var latDiff = (bounds[1][0] - bounds[0][0]) * 0.3;
+        var lonDiff = (bounds[1][1] - bounds[0][1]) * 0.3;
+        var extendedBounds = [
+            [bounds[0][0] - latDiff, bounds[0][1] - lonDiff],
+            [bounds[1][0] + latDiff, bounds[1][1] + lonDiff]
+        ];
 
-            myMap.setBounds(extendedBounds, { checkZoomRange: true, zoomMargin: 20 });
-        }
-    }).catch(function(error){
-        console.error("Ошибка геокодирования:", error);
-        // В случае ошибки оставляем резервный центр
-        myMap.setCenter([51.6608, 39.2003], 11);
+        // Устанавливаем расширенные границы на карту
+        myMap.setBounds(extendedBounds, { checkZoomRange: true, zoomMargin: 20 });
+
+        // Добавляем слой пробок с дорожными событиями
+        var trafficLayer = new ymaps.traffic.layer.Actual({
+            infoLayerShown: true
+        });
+        myMap.layers.add(trafficLayer);
+
+        // Автообновление пробок каждые 2 минуты
+        setInterval(function () {
+            trafficLayer.reload();
+        }, 120000); // 120000 мс = 2 минуты
     });
 }
