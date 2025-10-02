bb5855562423f497668c3ed92d7dad19b81bea81
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 00:47:57 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -1,15 +1,18 @@
 ymaps.ready(init);
 
 function init() {
+    // Создаем карту без центра и зума, с минималистичной тёмной темой
     var myMap = new ymaps.Map("map", {
         controls: [],
+        type: "yandex#map", // базовая карта
     });
 
+    // Получаем границы города Воронеж через геокодер
     ymaps.geocode("Воронеж", { results: 1 }).then(function (res) {
         var city = res.geoObjects.get(0);
         var bounds = city.properties.get('boundedBy');
 
-        // Расширяем границы на 30% с каждой стороны для показа окрестностей
+        // Расширяем границы на 30% для показа окрестностей и трасс
         var latDiff = (bounds[1][0] - bounds[0][0]) * 0.3;
         var lonDiff = (bounds[1][1] - bounds[0][1]) * 0.3;
         var extendedBounds = [
@@ -17,7 +20,11 @@ function init() {
             [bounds[1][0] + latDiff, bounds[1][1] + lonDiff]
         ];
 
+        // Устанавливаем расширенные границы карты
         myMap.setBounds(extendedBounds, { checkZoomRange: true, zoomMargin: 20 });
+
+        // Включаем тёмную тему
+        myMap.options.set('preset', 'islands#night');
 
         // Добавляем слой пробок с дорожными событиями
         var trafficLayer = new ymaps.traffic.layer.Actual({
