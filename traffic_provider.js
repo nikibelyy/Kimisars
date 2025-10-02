da2f6134842bb4b64d4d7d590aafd3bb7dc54015
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 01:59:21 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -3,24 +3,23 @@ ymaps.ready(init);
 function init() {
     // Создаем карту, центрируем на Воронеж
     var map = new ymaps.Map("map", {
-        center: [51.660781, 39.200296], // координаты центра Воронежа
+        center: [51.660781, 39.200296], // координаты Воронежа
         zoom: 12,
-        controls: ['zoomControl', 'trafficControl', 'typeSelector', 'fullscreenControl']
+        controls: [] // Убираем все кнопки
     });
 
-    // Добавляем слой пробок + событий ДТП
-    var trafficControl = new ymaps.control.TrafficControl({
-        state: {
-            // Показывать пробки
-            trafficShown: true,
-            // Показывать дорожные события (ДТП, ремонт и пр.)
-            infoLayerShown: true
-        }
-    });
+    // Функция подключения пробок и ДТП
+    function loadTraffic() {
+        var trafficProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
+        trafficProvider.setMap(map);
+    }
 
-    map.controls.add(trafficControl);
+    // Загружаем пробки и ДТП при старте
+    loadTraffic();
 
-    // Включаем слой пробок на карте сразу
-    var trafficProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
-    trafficProvider.setMap(map);
+    // Автообновление каждые 10 минут (600 000 мс)
+    setInterval(function () {
+        loadTraffic();
+        console.log("Данные о пробках и ДТП обновлены");
+    }, 600000);
 }
