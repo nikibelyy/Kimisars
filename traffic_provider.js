545a629042bc4e40a23e945fb25b43a0578de397
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 10:50:37 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -1,11 +1,38 @@
 ymaps.ready(init);
 
 function init() {
-    // Создаем карту, центрируем на Воронеж
     var map = new ymaps.Map("map", {
-        center: [51.660781, 39.200296], // координаты Воронежа
-        zoom: 12,
-        controls: [] // Убираем все кнопки
+        controls: [], // убираем кнопки
+        type: "yandex#map" // светлая тема
+    });
+
+    // Находим границы Воронежа
+    ymaps.geocode("Воронеж", { results: 1 }).then(function (res) {
+        var city = res.geoObjects.get(0);
+        var bounds = city.properties.get("boundedBy");
+
+        // Устанавливаем карту на границы города
+        map.setBounds(bounds, { checkZoomRange: true, zoomMargin: 20 });
+
+        // Оптимальное приближение (подбираем вручную)
+        map.events.add("boundschange", function once() {
+            var currentZoom = map.getZoom();
+
+            // Если слишком далеко (например, 10–11), приближаем
+            if (currentZoom < 12) {
+                map.setZoom(12);
+            }
+
+            // Если слишком близко, ограничиваем до 13
+            if (currentZoom > 13) {
+                map.setZoom(13);
+            }
+
+            map.events.remove("boundschange", once);
+        });
+
+        // Загружаем пробки и ДТП
+        loadTraffic();
     });
 
     // Функция подключения пробок и ДТП
@@ -14,10 +41,7 @@ function init() {
         trafficProvider.setMap(map);
     }
 
-    // Загружаем пробки и ДТП при старте
-    loadTraffic();
-
-    // Автообновление каждые 10 минут (600 000 мс)
+    // Автообновление каждые 15 минут (900 000 мс)
     setInterval(function () {
         loadTraffic();
         console.log("Данные о пробках и ДТП обновлены");
