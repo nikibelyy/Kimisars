6824f45b61b24349ff37975187341e3b2e5e2d8b
nikibelyy <ogurtsov.1999@gmail.com>
2025-10-03 11:25:19 +0300

    Update traffic_provider.js

--- traffic_provider.js
+++ traffic_provider.js
@@ -1,35 +1,26 @@
 ymaps.ready(init);
 
 function init() {
+    // Создаем карту, центрируем на Воронеж
     var myMap = new ymaps.Map("map", {
-        center: [51.6755, 39.2083], // центр Воронежа (примерный)
-        zoom: 12,
-        controls: [],
+        center: [51.6755, 39.2083], // координаты центра Воронежа
+        zoom: 12,                    // подходящий масштаб для города
+        controls: []
     });
 
-    // Получаем границы города Воронеж через геокодер
-    ymaps.geocode("Воронеж", { results: 1 }).then(function (res) {
-        var city = res.geoObjects.get(0);
-        var bounds = city.properties.get('boundedBy'); // [[southWest], [northEast]]
+    // Создаем провайдер пробок с включенными дорожными событиями
+    var actualProvider = new ymaps.traffic.provider.Actual(
+        {},
+        { infoLayerShown: true } // дорожные события
+    );
 
-        // Устанавливаем границы карты, чтобы отображался только город
-        myMap.setBounds(bounds, { checkZoomRange: true, zoomMargin: 20 });
+    // Добавляем провайдер на карту
+    actualProvider.setMap(myMap);
 
-        // Создаем провайдер пробок с дорожными событиями
-        var actualProvider = new ymaps.traffic.provider.Actual(
-            {},
-            { infoLayerShown: true } // включаем дорожные события
-        );
-
-        // Добавляем провайдер на карту
-        actualProvider.setMap(myMap);
-
-        // Создаем слой дорожных событий (инфоточек)
-        var infoLayer = new ymaps.traffic.InfoLayer(myMap, {
-            provider: actualProvider
-        });
-
-        // Включаем слой дорожных событий
-        infoLayer.setMap(myMap);
+    // Слой инфоточек для событий на дорогах
+    var infoLayer = new ymaps.traffic.InfoLayer(myMap, {
+        provider: actualProvider
     });
+
+    infoLayer.setMap(myMap);
 }
