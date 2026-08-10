b547b4d865851e065f980687ef399ab5cb311470
nikibelyy <ogurtsov.1999@gmail.com>
2026-08-10 03:02:50 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('nsis-v2').then((cache) => cache.addAll(['./index.html'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
