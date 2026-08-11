5ea455397537eb5bb0333bde77cd323ce2139fd3
nikibelyy <ogurtsov.1999@gmail.com>
2026-08-11 16:46:34 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('nsis-v2').then((cache) => cache.addAll(['./index.html'])));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
